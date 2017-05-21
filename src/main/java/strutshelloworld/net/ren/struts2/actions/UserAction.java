017e06accc7a2b1de0e656e47993e3f99fb3f995
kukuhutama <kukuh2utama@gmail.com>
2017-05-22 02:37:07 +0700

    Interceptors in progress

--- src/main/java/strutshelloworld/net/ren/struts2/actions/UserAction.java
+++ src/main/java/strutshelloworld/net/ren/struts2/actions/UserAction.java
@@ -13,70 +13,168 @@ import strutshelloworld.net.ren.struts2.service.UserService;
 /**
  * The Class UserAction.
  */
-public class UserAction extends ActionSupport implements ModelDriven, Preparable{
+public class UserAction extends ActionSupport implements ModelDriven, Preparable {
 
 	/** The user service. */
 	@Autowired
 	private UserService userService;
+
+	/** The is log in. */
+	private boolean isLogIn = true;
 	
+	/** The is sign up. */
+	private boolean isSignUp = false;
+	
+	/**
+	 * Checks if is sign up.
+	 *
+	 * @return true, if is sign up
+	 */
+	public boolean isSignUp() {
+		return isSignUp;
+	}
+
+	/**
+	 * Sets the sign up.
+	 *
+	 * @param isSignUp the new sign up
+	 */
+	public void setSignUp(boolean isSignUp) {
+		this.isSignUp = isSignUp;
+	}
+
+	/** The sign up message. */
+	private String signUpMessage;
+
+	/**
+	 * Gets the sign up message.
+	 *
+	 * @return the sign up message
+	 */
+	public String getSignUpMessage() {
+		return signUpMessage;
+	}
+
+	/**
+	 * Sets the sign up message.
+	 *
+	 * @param signUpMessage the new sign up message
+	 */
+	public void setSignUpMessage(String signUpMessage) {
+		this.signUpMessage = signUpMessage;
+	}
+
+	/**
+	 * Checks if is log in.
+	 *
+	 * @return true, if is log in
+	 */
+	public boolean isLogIn() {
+		return isLogIn;
+	}
+
+	/**
+	 * Sets the log in.
+	 *
+	 * @param isLogIn
+	 *            the new log in
+	 */
+	public void setLogIn(boolean isLogIn) {
+		this.isLogIn = isLogIn;
+	}
+
 	/** The user. */
 	private User user = new User();
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.opensymphony.xwork2.Preparable#prepare()
 	 */
 	public void prepare() throws Exception {
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.opensymphony.xwork2.ModelDriven#getModel()
 	 */
 	public Object getModel() {
 		return user;
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see com.opensymphony.xwork2.ActionSupport#execute()
 	 */
 	public String execute() throws Exception {
 		System.out.println("execute");
-		return INPUT;
+		return SUCCESS;
 	}
-	
+
 	/**
 	 * Sign up.
 	 *
 	 * @return the string
 	 */
-	public String SignUp(){
+	public String signUp() {
 		System.out.println("SignUp");
-		userService.addUser(user);
+		try {
+			userService.addUser(user);
+			isLogIn = true;
+	    	isSignUp = true;
+			signUpMessage = "Sign Up Success";
+		} catch (Exception ex) {
+			isLogIn = false;
+			return ERROR;
+		}
 		return SUCCESS;
 	}
-	
+
+	/**
+	 * Show sign up.
+	 *
+	 * @return the string
+	 */
+	public String showSignUp() {
+		System.out.println("showSignUp");
+		isLogIn = false;
+		return SUCCESS;
+	}
+
 	/**
 	 * Log in.
 	 *
 	 * @return the string
 	 */
-	public String LogIn(){
+	public String logIn() {
 		System.out.println("Login");
-		User logInUser = userService.findUserByUsernameAndPassword(user.getUsername(),user.getPassword());
-		if( logInUser.getUsername()== null || logInUser.getPassword() == null){
-		    return SUCCESS;
+		User logInUser = userService.findUserByUsernameAndPassword(user.getUsername(), user.getPassword());
+		if (logInUser.getUsername() == null || logInUser.getPassword() == null) {
+			return SUCCESS;
 		} else {
-		    addActionError(getText("error.login"));
-		    return ERROR;
+			addActionError(getText("error.login"));
+			return ERROR;
 		}
 	}
-	
+
+	/**
+	 * Show log in.
+	 *
+	 * @return the string
+	 */
+	public String showLogIn() {
+		System.out.println("ShowLogIn");
+		return INPUT;
+	}
+
 	/**
 	 * Log out.
 	 *
 	 * @return the string
 	 */
-	public String LogOut(){
-		
+	public String logOut() {
 		return SUCCESS;
 	}
 
--- src/main/webapp/WEB-INF/jsp/Login.jsp
+++ src/main/webapp/WEB-INF/jsp/Login.jsp
@@ -9,20 +9,23 @@
 <body>
 <h2>Struts 2 - Login Application</h2>
 <s:actionerror />
-<s:form action="login">
+<s:if test="!isSignUp()">
+   <h2><s:property value="signUpMessage"/>In here</h2>
+</s:if > 
+<s:form action="logIn">
 	<s:textfield name="username" key="label.username" size="20" />
 	<s:password name="password" key="label.password" size="20" />
-	<s:if test="%{model.id !=0}">
-		<s:submit method="SignUp" key="label.signup" align="center" />
+	
+	<s:if test="!isLogIn()">
+		<s:submit key="label.signup" align="center"  action="signUp"/>
 	</s:if>
     <s:else>
         <s:submit key="label.login" align="center" />
     </s:else>
 </s:form>
-<s:if test="%{model.id != 0}">
-<s:url var="signUpUrl" action="signup"/>
-<s:a href="%{signUpUrl}">Sign Up</s:a>
+<s:if test="isLogIn()">
+<s:url var="signUpUrl" action="showSignUp"/>
+Does not have account yet? <s:a href="%{signUpUrl}">Click Sign Up</s:a>
 </s:if>
-
 </body>
 </html>
